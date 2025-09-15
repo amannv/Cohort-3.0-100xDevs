1cf4e2e6b4db053e4ddd5bb46943ad628bbe61b6
Aman <aman.ver9345@gmail.com>
2025-09-15 23:50:59 +0530

    adde week 13 file

--- week-13.1-tailwind/src/App.jsx
+++ week-13.1-tailwind/src/App.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-import { Button } from "./components/button";
-import { InputBox } from "./components/input";
-import { Otp } from "./components/otp";
 
 function App() {
 
+  const [sideBarOpen, setSideBarOpen] = useState(true);
+
   return (
-    <div className="h-screen bg-blue-800">
-      {/* <InputBox type={Text} placeholder={"write your text here"}></InputBox>
-      <Button disabled={true}>Signup</Button> */}
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Otp />
+    <div className="flex">
+      <SideBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen}/>
+      <MainContent sideBarOpen={sideBarOpen} />
+    </div>
+  )
+}
+
+const SideBar = ({sideBarOpen, setSideBarOpen}) => {
+  return (
+    <div>
+      <div className="w-80 h-screen bg-red-100"></div>
+    </div>
+  )
+}
+
+const MainContent = () => {
+  return (
+    <div className="w-full">
+      <div className="h-42 mb-4 bg-black"></div>
+    <div className="grid grid-cols-11 gap-4">
+      <div className="h-64 ml-4 p-8 rounded-2xl -translate-y-24 bg-red-200 col-span-2 shadow-xl"></div>
+      <div className="h-96 rounded-2xl bg-green-200 col-span-6 shadow-xl"></div>
+      <div className="h-96 rounded-2xl mr-4 bg-yellow-200 col-span-3 shadow-xl"></div>
+
+    </div>
     </div>
   )
 }
---
+++ week-13.1-tailwind/src/answers/1basic-project.jsx
@@ -0,0 +1,15 @@
+export const SidebarClass1 = () => {
+
+    return (
+        <div className="flex">
+            <div className="transition-all ease-in-out duration-2000 bg-red-200 md:w-96 w-0  h-screen ">
+                SideBar
+            </div>
+            <div className="bg-green-200 w-full h-screen">
+                Content
+            </div>
+
+        </div>
+    )
+
+}
